0e8f46ca14b7e82db6335080485e4c2cf35468e8
Imperial79 <avishekverma79@gmail.com>
2024-11-21 18:15:36 +0530

    version upgraded

--- lib/Components/constants.dart
+++ lib/Components/constants.dart
@@ -5,8 +5,8 @@ import 'dart:convert';
 
 import 'package:intl/intl.dart';
 
-const kAppVersion = "1.1.1";
-const kAppBuild = "10";
+const kAppVersion = "1.1.2";
+const kAppBuild = "12";
 
 const String kImagePath = "assets/images";
 const String welcomeImages = "$kImagePath/welcome-images";
